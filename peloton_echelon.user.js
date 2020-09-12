06f4f7a1f47ca0a3a1b55fc931ff3b571e5afa9f
Min Hur <minhur@gmail.com>
2020-09-12 02:43:15 -0700

    Update peloton_echelon.user.js

--- peloton_echelon.user.js
+++ peloton_echelon.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Peleton to Echelon
-// @version      3.2
+// @version      4.0
 // @author       minhur
 // @match        https://members.onepeloton.com/*
 // @grant        none
